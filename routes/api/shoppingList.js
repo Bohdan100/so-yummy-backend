de27ebd18016d0eb85fc229daf320473453f86cb
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2023-04-01 11:38:58 +0300

    add shoppingList router

---
+++ routes/api/shoppingList.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const { auth, validateBody } = require("../../middlewares");
+// const { addShoppingListSchema } = require('../../schemas');
+const { shoppingList: ctrl } = require("../../controllers");
+
+router.get("/", auth, ctrl.getShoppingList);
+
+router.post(
+  "/",
+  auth,
+  // validateBody(addShoppingListSchema),
+  ctrl.addShoppingList
+);
+
+router.delete("/:id", isAuth, ctrl.deleteShoppingList);
+
+module.exports = router;
