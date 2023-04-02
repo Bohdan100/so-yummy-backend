6d4bdb744543b889946c6215e0e6a990fc5d47a4
Aleksey Knigin <alexyo2626@gmail.com>
2023-04-02 04:28:13 +0300

    add router ingredients

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,13 @@
-const recipesRouter = require('./recipes');
-const favoriteRouter = require('./favorite');
-const shoppingListRouter = require('./shoppingList');
-const ownRecipesRouter = require('./ownRecipes');
+const recipesRouter = require("./recipes");
+const favoriteRouter = require("./favorite");
+const shoppingListRouter = require("./shoppingList");
+const ownRecipesRouter = require("./ownRecipes");
+const ingredientsRouter = require("./ingredients");
 
 module.exports = {
   recipesRouter,
   favoriteRouter,
   shoppingListRouter,
   ownRecipesRouter,
+  ingredientsRouter,
 };
