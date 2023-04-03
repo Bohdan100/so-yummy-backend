24b9dacdad4b829bda2bee15903946bb5a6b58f0
Nelia Bochenkova <nelya.teslenko.95@gmail.com>
2023-04-03 22:47:51 +0300

    update getOwnerRecipe

--- controllers/ownRecipes/getOwnRecipes.js
+++ controllers/ownRecipes/getOwnRecipes.js
@@ -1,15 +1,36 @@
 const { Recipe } = require('../../models');
+const { setPaginationSlice, HttpError } = require('../../helpers');
 
 const getRecipes = async (req, res) => {
   const userId = req?.user?._id;
+  const { page = 1, limit = 4 } = req.query;
   const ownRecipes = await Recipe.find({ owner: userId });
 
+  if (ownRecipes.length === 0) {
+    return res.json({
+      status: 'success',
+      code: 200,
+      data: {
+        result: ownRecipes,
+      },
+      total: 0,
+    });
+  }
+
+  const pagination = setPaginationSlice(page, limit, ownRecipes.length);
+  if (!pagination) {
+    throw HttpError(400, 'Incorrect pagination params');
+  }
+
   res.json({
-    status: 'succes',
+    status: 'success',
     code: 200,
     data: {
-      result: ownRecipes,
+      result: ownRecipes.slice(pagination.start, pagination.end),
     },
+    total: ownRecipes.length,
+    page,
+    limit,
   });
 };
 
