66692fae162eec88f20215ee2b1668c536cc87cc
Nelia Bochenkova <nelya.teslenko.95@gmail.com>
2023-04-04 19:56:19 +0300

    add getOwnRecipeById

--- controllers/ownRecipes/index.js
+++ controllers/ownRecipes/index.js
@@ -1,10 +1,12 @@
 const getOwnRecipes = require('./getOwnRecipes');
 const addOwnRecipe = require('./addOwnRecipe');
 const deleteOwnRecipe = require('./deleteOwnRecipe');
+const getOwnRecipeById = require('./getOwnRecipeById');
 const { ctrlWrapper } = require('../../helpers');
 
 module.exports = {
   getOwnRecipes: ctrlWrapper(getOwnRecipes),
   addOwnRecipe: ctrlWrapper(addOwnRecipe),
   deleteOwnRecipe: ctrlWrapper(deleteOwnRecipe),
+  getOwnRecipeById: ctrlWrapper(getOwnRecipeById),
 };
