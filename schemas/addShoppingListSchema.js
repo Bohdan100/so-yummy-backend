d76671e3d609b43712796ba79bf22215e2229f71
Bohdan <104660586+Bohdan100@users.noreply.github.com>
2023-04-05 14:29:27 +0300

    shopping-list min max

--- schemas/addShoppingListSchema.js
+++ schemas/addShoppingListSchema.js
@@ -1,8 +1,8 @@
 const Joi = require("joi");
 
 const addShoppingListSchema = Joi.object().keys({
-  strIngredient: Joi.string().required(),
-  weight: Joi.string().required(),
+  strIngredient: Joi.string().required().min(2).max(35),
+  weight: Joi.string().required().min(2).max(10),
   image: Joi.string().required(),
   recipeId: Joi.string().required(),
 });
