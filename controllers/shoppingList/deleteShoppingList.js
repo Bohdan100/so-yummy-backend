a0ecf62489c2b0bb958992e17b53e28ccc933dd5
Bohdan <104660586+Bohdan100@users.noreply.github.com>
2023-04-05 17:00:55 +0300

    shop list errors

--- controllers/shoppingList/deleteShoppingList.js
+++ controllers/shoppingList/deleteShoppingList.js
@@ -5,9 +5,9 @@ const deleteShoppingList = async (req, res, next) => {
   const { id } = req.params;
   const result = await ShoppingList.findByIdAndRemove(id);
   if (!result) {
-    throw HttpError(404, "Not found");
+    next(HttpError(404, "Not found"));
   }
-  res.json({ message: "shoping list deleted" });
+  res.json({ message: "Success remove ingredient from shoping list" });
 };
 
 module.exports = deleteShoppingList;
