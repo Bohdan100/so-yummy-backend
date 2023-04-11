56490b315d24f9526bd66c856516c4adf6207e10
Bohdan <104660586+Bohdan100@users.noreply.github.com>
2023-04-11 22:50:52 +0300

    google

---
+++ controllers/auth/signupGoogle.js
@@ -0,0 +1,83 @@
+const queryString = require("node:querystring");
+const axios = require("axios");
+
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { User } = require("../../models");
+const { HttpError } = require("../../helpers");
+const {
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  BASE_URL,
+  SECRET_KEY,
+  FRONTEND_URL,
+} = process.env;
+
+const googleAuth = async (req, res) => {
+  const stringifiedParams = queryString.stringify({
+    client_id: GOOGLE_CLIENT_ID,
+    redirect_uri: `${BASE_URL}/api/auth/google-redirect`,
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile",
+    ].join(" "),
+    response_type: "code",
+    access_type: "offline",
+    prompt: "consent",
+  });
+
+  return res.redirect(
+    `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`
+  );
+};
+
+const googleRedirect = async (req, res) => {
+  const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
+  const urlObj = new URL(fullUrl);
+  const urlParams = queryString.parse(urlObj.search.substring(1));
+  const code = urlParams.code;
+  const tokenData = await axios({
+    url: `https://oauth2.googleapis.com/token`,
+    method: "post",
+    data: {
+      client_id: GOOGLE_CLIENT_ID,
+      client_secret: GOOGLE_CLIENT_SECRET,
+      redirect_uri: `${BASE_URL}/api/auth/google-redirect`,
+      grant_type: "authorization_code",
+      code,
+    },
+  });
+
+  const userData = await axios({
+    url: "https://www.googleapis.com/oauth2/v2/userinfo",
+    method: "get",
+    headers: {
+      Authorization: `Bearer ${tokenData.data.access_token}`,
+    },
+  });
+
+  const email = userData.data.email;
+  const name = userData.data.given_name;
+
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
+  const password = `Back${userData.data.email}`;
+  const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+
+  await User.create({ name, email, password: hashPassword });
+  const newUser = await User.findOne({ email });
+  const payload = {
+    id: newUser._id,
+  };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+  newUser.token = token;
+  await newUser.save();
+
+  return res.redirect(
+    `${FRONTEND_URL}/google-redirect/?token=${token}&email=${email}&name=${name}&avatar=${newUser.avatar}`
+  );
+};
+
+module.exports = { googleAuth, googleRedirect };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,15 +1,28 @@
-const express = require('express');
-const { auth: ctrl } = require('../../controllers');
-const { auth, validateBody } = require('../../middlewares');
-const { userJoiRegisterSchema, userJoiLoginSchema, userJoiSchemaUpdate } = require('../../schemas');
-const { uploadCloud } = require('../../middlewares/uploadAvatar');
+const express = require("express");
+const { auth: ctrl } = require("../../controllers");
+const { auth, validateBody } = require("../../middlewares");
+const {
+  userJoiRegisterSchema,
+  userJoiLoginSchema,
+  userJoiSchemaUpdate,
+} = require("../../schemas");
+const { uploadCloud } = require("../../middlewares/uploadAvatar");
 
 const router = express.Router();
 
-router.post('/signup', validateBody(userJoiRegisterSchema), ctrl.signup);
-router.post('/login', validateBody(userJoiLoginSchema), ctrl.login);
-router.get('/current', auth, ctrl.getCurrentUser);
-router.get('/logout', auth, ctrl.logout);
-router.patch('/edit', auth, uploadCloud, validateBody(userJoiSchemaUpdate), ctrl.updateUser);
+router.post("/signup", validateBody(userJoiRegisterSchema), ctrl.signup);
+router.post("/login", validateBody(userJoiLoginSchema), ctrl.login);
+router.get("/current", auth, ctrl.getCurrentUser);
+router.get("/logout", auth, ctrl.logout);
+router.patch(
+  "/edit",
+  auth,
+  uploadCloud,
+  validateBody(userJoiSchemaUpdate),
+  ctrl.updateUser
+);
+
+router.get("/google", ctrl.googleAuth);
+router.get("/google-redirect", ctrl.googleRedirect);
 
 module.exports = router;
