1d8688bae80f50424db4c18d5337c073febb0a86
Bohdan <104660586+Bohdan100@users.noreply.github.com>
2023-04-12 17:13:01 +0300

    fixed

--- controllers/favorite/getFavorites.js
+++ controllers/favorite/getFavorites.js
@@ -1,30 +1,5 @@
 const { UserFavorite } = require("../../models");
 
-// ? 1 - ВАРИАНТ - пока не удалять
-// const getFavorites = async (req, res) => {
-// const { _id: userId } = req.user;
-//   const { page = 1, limit = 4 } = req.query;
-//   const skip = (page - 1) * limit;
-
-//   const favoriteRecipesByUserId = await UserFavorite.find({ userId }, '', {
-//     skip,
-//     limit: Number(limit),
-//   }).populate('recipe', '_id title category description thumb preview time');
-
-//   res.json({
-//     status: 'succes',
-//     code: 200,
-//     data: {
-//       result: favoriteRecipesByUserId,
-//     },
-//     page,
-//     limit,
-//   });
-// };
-
-// module.exports = getFavorites;
-
-// 2 - ВАРИАНТ;
 const { setPaginationSlice } = require("../../helpers");
 
 const getFavorites = async (req, res) => {
@@ -60,7 +35,7 @@ const getFavorites = async (req, res) => {
     });
   }
 
-  res.json({
+  return res.json({
     status: "success",
     code: 200,
     data: {
