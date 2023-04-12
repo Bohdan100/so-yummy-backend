42f1d0e0db3746716989ee998ba0fa8b9fcd1e09
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2023-04-13 01:50:45 +0300

    fix

--- controllers/shoppingList/deleteShoppingList.js
+++ controllers/shoppingList/deleteShoppingList.js
@@ -7,7 +7,13 @@ const deleteShoppingList = async (req, res, next) => {
   if (!result) {
     next(HttpError(404, "Not found"));
   }
-  res.json({ message: "Success remove ingredient from shoping list" });
+  console.log(result);
+  res.json({
+    status: "succes",
+    code: 200,
+    id,
+    message: `Success remove ingredient with id - ${id} from shoping list`,
+  });
 };
 
 module.exports = deleteShoppingList;
