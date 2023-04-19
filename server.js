96fa9435ae6755c21faeb33b4e1043353f017c52
Bohdan <104660586+Bohdan100@users.noreply.github.com>
2023-04-20 02:10:25 +0300

    console

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
+  .then(console.log("DB_HOST", DB_HOST))
   .then(
     app.listen(PORT, () => {
       console.log("Database connection successful");
